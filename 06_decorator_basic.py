3fc3567c65ba6ae9747263d082ad4d701f97e005
dev <dev_ankush@yahoo.com>
2026-03-15 10:33:43 +1100

    decorator recap

--- 06_decorator_basic.py
+++ 06_decorator_basic.py
@@ -3,7 +3,7 @@ import functools
 def jsonify_decorator(func):
     functools.wraps(func)
     def modifyOutput(*args, **kwargs):
-        print(args)
+        print(args, type(args))
         return {"output": func(*args, **kwargs)}
     return modifyOutput
 
